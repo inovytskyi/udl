f1f6e5fa50305e7d10ebb70e182910fa0c73c927
Ivan Novytskyi <begibober@gmail.com>
2023-10-16 23:36:17 +0300

    fix image error

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,10 +1,9 @@
-import logoPic from "./_images/drone_logo.svg";
 import Image from "next/image";
 
 function NavBar() {
   return (
     <nav className="flex flex-nowrap items-center justify-start gap-2 border-b-2 border-blue-800 bg-[#2e026d] p-2 text-slate-200">
-      <Image src={logoPic} alt="UDL Logo" className="h-10 w-10" />
+      <Image src="/drone_logo.svg" alt="UDL Logo" width="32" height="32" />
       <div className="">NavBar</div>
     </nav>
   );
